05c8274ffa6e51e46e59abc992a30ad4037292c7
mrFred489 <frederik.lauridsen@gmail.com>
2018-12-21 10:28:48 +0100

    Completed day 21. Tried to write the elf code as python, did not succeed.

---
+++ 21.py
@@ -0,0 +1,145 @@
+from collections import *
+import itertools
+import random
+import sys
+import re
+
+f = open("21.txt").read().split("\n")
+
+
+def addr(reg, a, b, c):
+    reg[c] = reg[a] + reg[b]
+    return reg
+
+
+def addi(reg, a, b, c):
+    reg[c] = reg[a] + b
+    return reg
+
+
+def mulr(reg, a, b, c):
+    reg[c] = reg[a] * reg[b]
+    return reg
+
+
+def muli(reg, a, b, c):
+    reg[c] = reg[a] * b
+    return reg
+
+
+def banr(reg, a, b, c):
+    reg[c] = reg[a] & reg[b]
+    return reg
+
+
+def bani(reg, a, b, c):
+    reg[c] = reg[a] & b
+    return reg
+
+
+def borr(reg, a, b, c):
+    reg[c] = reg[a] | reg[b]
+    return reg
+
+
+def bori(reg, a, b, c):
+    reg[c] = reg[a] | b
+    return reg
+
+
+def setr(reg, a, b, c):
+    reg[c] = reg[a]
+    return reg
+
+
+def seti(reg, a, b, c):
+    reg[c] = a
+    return reg
+
+
+def gtir(reg, a, b, c):
+    reg[c] = int(a > reg[b])
+    return reg
+
+
+def gtri(reg, a, b, c):
+    reg[c] = int(reg[a] > b)
+    return reg
+
+
+def gtrr(reg, a, b, c):
+    reg[c] = int(reg[a] > reg[b])
+    return reg
+
+
+def eqir(reg, a, b, c):
+    reg[c] = int(a == reg[b])
+    return reg
+
+
+def eqri(reg, a, b, c):
+    reg[c] = int(reg[a] == b)
+    return reg
+
+
+def eqrr(reg, a, b, c):
+    reg[c] = int(reg[a] == reg[b])
+    return reg
+
+
+
+
+
+functions = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
+functions_index = ["addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr"]
+
+ip = 0
+
+ip_reg = int(re.findall("\d", f[0])[0])
+print("ip_reg", ip_reg)
+
+instructions = []
+for line in f[1:]:
+    if line == "":
+        continue
+    line = line.split(" ")
+    instructions.append((functions[functions_index.index(line[0])], int(line[1]), int(line[2]), int(line[3])))
+
+start_val = 5970143
+found_it = False
+loop_val = 1e10
+testing = True
+reg1s = set()
+ip = 0
+registers = [0 for _ in range(6)]
+registers[0] = start_val
+i = 0
+part1 = False
+prev_reg1 = 0
+while True:
+    func, a, b, c = instructions[ip]
+    if testing:
+        print(ip, func.__name__, registers)
+        if i > 200:
+            found_it = True
+            break
+    registers = func(registers, a, b, c)
+    registers[ip_reg] += 1
+    ip = registers[ip_reg]
+    if ip == 28:
+        if part1:
+            print(register[1])
+            break
+        if registers[1] in reg1s:
+            print(prev_reg1)
+            print("done")
+            break
+        else:
+            reg1s.add(registers[1])
+            prev_reg1 = registers[1]
+    if i % 10000000 == 0:
+        print(i, ip, registers)
+    i += 1
+# 16704546
+            
+print(start_val)
---
+++ 21annotatecode.py
@@ -0,0 +1,45 @@
+starting = True
+reg0 = 0
+reg1 = 0
+reg2 = 0
+reg3 = 0
+reg4 = 0
+reg5 = 0
+reg1s = []
+print(reg1, reg2, reg3, reg4, reg5)
+while True:
+    if starting:
+        reg5 = reg1 | 65536
+        reg1 = 8586263
+        starting = False
+    reg2 = reg5 & 255
+    reg1 += reg2
+    reg1 &= 16777215
+    reg1 *= 65899
+    reg1 &= 16777215
+    print("line 20", reg1, reg2, reg3, reg4, reg5)
+    if 256 > reg5:
+        print(reg1, reg2, reg3, reg4, reg5)
+        if reg1 in reg1s:
+            print(reg1s[-5:], reg1, reg1s[reg1s.index(reg1)-1], reg1s[reg1s.index(reg1)-3: reg1s.index(reg1)+3])
+            print("done")
+            break
+        else:
+            reg1s.append(reg1)
+            
+        if reg1 == reg0: break
+        else:
+            starting = True
+            continue
+    reg2 = 0
+    while not reg3 * 256 > reg5:
+        # print("line 35", reg1, reg2, reg3, reg4, reg5)
+
+        reg3 = reg2 + 1
+        reg3 *= 256
+        reg2 += 1
+    reg5 = reg2
+
+# 10508527
+# 13151574
+
